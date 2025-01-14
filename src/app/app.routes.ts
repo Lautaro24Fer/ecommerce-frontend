28c2c13d36b542fc3cc50605cfad80191af1dbdd
lautaro <lautaroleonelfernandez3@gmail.com>
2025-01-14 20:56:50 -0300

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/landing/landing.component').then(r => r.LandingComponent)
+  },
+  {
+    path: 'products',
+    loadComponent: () => import('./pages/products/products.component').then(r => r.ProductsComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
