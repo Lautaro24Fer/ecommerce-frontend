9379f9b1ebe8733536779f2942bc3b384d3511e6
lautaro <lautaroleonelfernandez3@gmail.com>
2025-01-18 03:26:21 -0300

    landing) carrousel de tarjetas + footer

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProductCardComponent } from "../../shared/product-card/product-card.component";
+import { ProductsCarrouselComponent } from "../../shared/products-carrousel/products-carrousel.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink, ProductCardComponent],
+  imports: [RouterLink, ProductsCarrouselComponent, FooterComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
--- src/app/shared/products-carrousel/products-carrousel.component.ts
+++ src/app/shared/products-carrousel/products-carrousel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from "../product-card/product-card.component";
 
 @Component({
   selector: 'app-products-carrousel',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './products-carrousel.component.html',
   styleUrl: './products-carrousel.component.css'
 })
