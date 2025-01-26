01a1b7c70349e3039430941f100a3de68aa46b0a
lautaro <lautaroleonelfernandez3@gmail.com>
2025-01-26 16:59:46 -0300

    landing) seccion segundo modelo✔

--- src/app/pages/landing/sections/second-model/second-model.component.ts
+++ src/app/pages/landing/sections/second-model/second-model.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BubblHoverCardComponent } from "../../../../shared/bubbl-hover-card/bubbl-hover-card.component";
 import { BubblSecondModelPosition } from '../../../../models/bubblSecondModelPosition';
 import { Product } from '../../../../models/product';
@@ -11,22 +11,41 @@ import { MockDataService } from '../../../../services/mock-data.service';
   templateUrl: './second-model.component.html',
   styleUrl: './second-model.component.css'
 })
-export class SecondModelComponent {
 
-  modelProducts: Product[];
+export class SecondModelComponent implements OnInit {
 
-  constructor(private readonly dataService: MockDataService){
-    this.modelProducts = dataService.getMockedData().filter(product => {
+  modelProducts!: Product[];
+
+  constructor(private readonly dataService: MockDataService){}
+  ngOnInit(): void {
+    this.modelProducts = this.dataService.getMockedData().filter(product => {
       const productsDisplayed: string[] = [
         'Camiseta Atletico Madrid',
         'Short Atletico Madrid',
         'Botines Future 2.1 Netfit'
       ]
-      if(!productsDisplayed.includes(product.name)){
+      if(productsDisplayed.includes(product.name)){
         return product;
       }
+      return false;
     })
+
+    
   }
 
-  
+  getProduct(name: string, recourse: string){
+    const product = this.modelProducts.find(p => p.name.toLowerCase() === name.toLowerCase());
+
+    switch(recourse){
+      case 'image':
+        return product?.imageUrl
+      break;
+      case 'productUrl':
+        return product?.imageUrl
+      break;
+      default: 
+        return ''
+      break;
+    }
+  }
 }
